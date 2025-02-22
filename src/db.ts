995c7bb0dd377d84c61b425004ce94f8414fe1f9
Sumit <sumitsongh160@gmail.com>
2025-02-23 00:52:29 +0530

    Initial setup of backend

---
+++ src/db.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+const objectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    password : {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+})
+
+
+const userModel =  model('users',userSchema);
+
+export{ userModel as users};
